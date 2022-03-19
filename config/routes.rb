052b721daa58d880e594682052f05ef1dcb8ad90
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-19 11:08:38 +0800

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :doctors do
     resources :prescriptions
   end
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
