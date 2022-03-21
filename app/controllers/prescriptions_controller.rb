a9058a498f41e8cef350c51a4145e0ceeafca584
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-21 19:19:00 +0800

    Add index, show, edit, update, and destroy to get_doctor parameters Edit index method of getting @prescriptions by using @doctor Declared variable presc_medicines that gets all presc_medicines under prescription in show Update paths in create and update method Add code to destroy fields of presc_medicines using find each and update redirect path of destroy Added id of presc_medicines as strong parameter

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,31 +1,29 @@
 class PrescriptionsController < ApplicationController
   before_action :set_prescription, only: %i[show edit update destroy]
-  before_action :get_doctor, only: %i[new create]
+  before_action :get_doctor, only: %i[index show new create edit update destroy]
 
-  # GET /prescriptions or /prescriptions.json
   def index
-    @prescriptions = Prescription.all
+    @prescriptions = @doctor.prescriptions
   end
 
-  # GET /prescriptions/1 or /prescriptions/1.json
-  def show; end
+  def show
+    @presc_medicines = @prescription.presc_medicines
+  end
 
-  # GET /prescriptions/new
   def new
     @prescription = @doctor.prescriptions.build
+    # @presc_medicine = @prescription.presc_medicines.build
   end
 
-  # GET /prescriptions/1/edit
   def edit; end
 
-  # POST /prescriptions or /prescriptions.json
   def create
     @prescription = @doctor.prescriptions.build(prescription_params)
 
     respond_to do |format|
       if @prescription.save
         format.html do
-          redirect_to doctor_prescription_url(@prescription), notice: 'Prescription was successfully created.'
+          redirect_to doctor_prescriptions_path(@doctor), notice: 'Prescription was successfully created.'
         end
         format.json { render :show, status: :created, location: @prescription }
       else
@@ -40,7 +38,7 @@ class PrescriptionsController < ApplicationController
     respond_to do |format|
       if @prescription.update(prescription_params)
         format.html do
-          redirect_to doctor_prescription_url(@prescription), notice: 'Prescription was successfully updated.'
+          redirect_to doctor_prescriptions_path(@doctor), notice: 'Prescription was successfully updated.'
         end
         format.json { render :show, status: :ok, location: @prescription }
       else
@@ -50,12 +48,12 @@ class PrescriptionsController < ApplicationController
     end
   end
 
-  # DELETE /prescriptions/1 or /prescriptions/1.json
   def destroy
+    @prescription.presc_medicines.find_each(&:destroy)
     @prescription.destroy
 
     respond_to do |format|
-      format.html { redirect_to doctor_prescriptions_url, notice: 'Prescription was successfully destroyed.' }
+      format.html { redirect_to doctor_prescriptions_path(@doctor), notice: 'Prescription was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -66,14 +64,12 @@ class PrescriptionsController < ApplicationController
     @doctor = Doctor.find(params[:doctor_id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_prescription
     @prescription = Prescription.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def prescription_params
     params.require(:prescription).permit(:total_cost, :doctor_id,
-                                         presc_medicines_attributes: %i[prescription_id medicine_id _destroy])
+                                         presc_medicines_attributes: %i[id prescription_id medicine_id _destroy])
   end
 end
