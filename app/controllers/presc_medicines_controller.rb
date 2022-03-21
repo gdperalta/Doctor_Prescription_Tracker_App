c6de2fdf2021e99ad215c8992ffdd9421fbed5db
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-21 18:55:36 +0800

    Commented added controllers and routes for presc_medicines. Codes were not necessary to create the foreign keys

--- app/controllers/presc_medicines_controller.rb
+++ app/controllers/presc_medicines_controller.rb
@@ -1,42 +1,42 @@
 class PrescMedicinesController < ApplicationController
-  before_action :set_presc_medicine, only: %i[edit update destroy]
+  # before_action :set_presc_medicine, only: %i[edit update destroy]
 
-  def new
-    @presc_medicine = Presc_medicine.new
-  end
+  # def new
+  #   @presc_medicine = Presc_medicine.new
+  # end
 
-  def edit; end
+  # def edit; end
 
-  def create
-    @presc_medicine = Presc_medicine.new(presc_medicine_params)
+  # def create
+  #   @presc_medicine = Presc_medicine.new(presc_medicine_params)
 
-    if @presc_medicine.save
-      nil
-    else
-      render :new
-    end
-  end
+  #   if @presc_medicine.save
+  #     nil
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def update
-    if @presc_medicine.update(presc_medicine_params)
-      redirect_to doctor_prescription_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @presc_medicine.update(presc_medicine_params)
+  #     redirect_to doctor_prescription_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    @presc_medicine.destroy
-    redirect_to doctor_prescription_path
-  end
+  # def destroy
+  #   @presc_medicine.destroy
+  #   redirect_to doctor_prescription_path
+  # end
 
-  private
+  # private
 
-  def set_presc_medicine
-    @presc_medicine = Presc_medicine.find(params[:id])
-  end
+  # def set_presc_medicine
+  #   @presc_medicine = Presc_medicine.find(params[:id])
+  # end
 
-  def presc_medicine_params
-    params.require(:presc_medicines).permit(:prescription_id, :medicine_id)
-  end
+  # def presc_medicine_params
+  #   params.require(:presc_medicines).permit(:prescription_id, :medicine_id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :medicines
   resources :doctors do
-    resources :prescriptions
+    resources :prescriptions do
+      # get 'presc_medicines/new', to: 'presc_medicines#new', as: 'new_presc_medicines'
+      # post 'presc_medicines', to: 'presc_medicines#create'
+      # get 'presc_medicines/:id/edit', to: 'presc_medicines#edit', as: 'edit_presc_medicines'
+      # patch 'presc_medicines/:id', to: 'presc_medicines#update'
+      # delete 'presc_medicines/:id', to: 'presc_medicines#destroy'
+    end
   end
 
   root 'home#index'
