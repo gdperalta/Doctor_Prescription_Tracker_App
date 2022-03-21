75cee8e9b0c151eb20f1eac1aa14403a049cfafc
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-21 12:56:11 +0800

    Updated controller paths and add methods Add get_doctor method to find corresponding Doctor of the prescription Add before_action for new and create for the method get_doctor Changed new and create by associating creation of  prescription with a doctor Corrected the paths so they are in line with nested route under doctor Add presc_medicines_attributes in permitted attributes

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,5 +1,6 @@
 class PrescriptionsController < ApplicationController
-  before_action :set_prescription, only: %i[ show edit update destroy ]
+  before_action :set_prescription, only: %i[show edit update destroy]
+  before_action :get_doctor, only: %i[new create]
 
   # GET /prescriptions or /prescriptions.json
   def index
@@ -7,25 +8,25 @@ class PrescriptionsController < ApplicationController
   end
 
   # GET /prescriptions/1 or /prescriptions/1.json
-  def show
-  end
+  def show; end
 
   # GET /prescriptions/new
   def new
-    @prescription = Prescription.new
+    @prescription = @doctor.prescriptions.build
   end
 
   # GET /prescriptions/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /prescriptions or /prescriptions.json
   def create
-    @prescription = Prescription.new(prescription_params)
+    @prescription = @doctor.prescriptions.build(prescription_params)
 
     respond_to do |format|
       if @prescription.save
-        format.html { redirect_to prescription_url(@prescription), notice: "Prescription was successfully created." }
+        format.html do
+          redirect_to doctor_prescription_url(@prescription), notice: 'Prescription was successfully created.'
+        end
         format.json { render :show, status: :created, location: @prescription }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,9 @@ class PrescriptionsController < ApplicationController
   def update
     respond_to do |format|
       if @prescription.update(prescription_params)
-        format.html { redirect_to prescription_url(@prescription), notice: "Prescription was successfully updated." }
+        format.html do
+          redirect_to doctor_prescription_url(@prescription), notice: 'Prescription was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @prescription }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +55,25 @@ class PrescriptionsController < ApplicationController
     @prescription.destroy
 
     respond_to do |format|
-      format.html { redirect_to prescriptions_url, notice: "Prescription was successfully destroyed." }
+      format.html { redirect_to doctor_prescriptions_url, notice: 'Prescription was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_prescription
-      @prescription = Prescription.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def prescription_params
-      params.require(:prescription).permit(:total_cost, :doctor_id)
-    end
+  def get_doctor
+    @doctor = Doctor.find(params[:doctor_id])
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_prescription
+    @prescription = Prescription.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def prescription_params
+    params.require(:prescription).permit(:total_cost, :doctor_id,
+                                         presc_medicines_attributes: %i[prescription_id medicine_id _destroy])
+  end
 end
