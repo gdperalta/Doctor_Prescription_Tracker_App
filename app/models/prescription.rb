ccf77dc88de9a90aac5b998ee9f3d83385fc2f21
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-21 12:51:02 +0800

    Add function accepts_nested_attributes_for presc_medicines in order to nest in form

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,4 +1,5 @@
 class Prescription < ApplicationRecord
   belongs_to :doctor
   has_many :presc_medicines
+  accepts_nested_attributes_for :presc_medicines, allow_destroy: true, reject_if: :all_blank
 end
