297ebebc3b97cfcfdb79c098d4af1b7ad5c302b0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-23 16:07:33 +0800

    Add showDiscountValue and changeDiscountValue functions add showDiscountValue as callback in getTotalCost Add eventListener to discount_output - changeDiscountValue

--- app/javascript/packs/getCost.js
+++ app/javascript/packs/getCost.js
@@ -1,3 +1,19 @@
+const showDiscountValue = () => {
+	const discounts = document.querySelectorAll('.discount_fields');
+	const discountOutput = document.querySelectorAll('.discount_output');
+	const l = discounts.length;
+
+	for (let i = 0; i < l; i++) {
+		discountOutput[i].value = discounts[i].value;
+	}
+};
+
+const changeDiscountValue = (e) => {
+	let discountField = e.target.previousElementSibling;
+	discountField.value = e.target.value;
+	getTotalCost();
+};
+
 export const getTotalCost = () => {
 	const medicines = document.querySelectorAll('.input_meds');
 	const discounts = document.querySelectorAll('.discount_fields');
@@ -12,17 +28,23 @@ export const getTotalCost = () => {
 			(1 - discounts[index].value / 100);
 	});
 	totalCost.value = total;
+
+	showDiscountValue();
 };
 
 export const handleEvents = () => {
 	const medicines = document.querySelectorAll('.input_meds');
 	const discounts = document.querySelectorAll('.discount_fields');
+	const discountOutput = document.querySelectorAll('.discount_output');
 	const l = medicines.length;
 
 	for (let i = 0; i < l; i++) {
 		if (!medicines[i].onchange) {
 			medicines[i].onchange = getTotalCost;
 			discounts[i].onchange = getTotalCost;
+			discountOutput[i].oninput = () => {
+				changeDiscountValue(event);
+			};
 		}
 	}
 };
