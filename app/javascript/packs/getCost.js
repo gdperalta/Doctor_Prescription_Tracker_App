72fb893b0645d358525f06fdc06147480650d3b5
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-23 15:35:19 +0800

    Add codes to handle computing the discounted price Add condition to check if elements are in the page in order to execture Removed updateCost function Add handleEvents function to add events to input_meds and discount_fields elements

--- app/javascript/packs/getCost.js
+++ app/javascript/packs/getCost.js
@@ -1,15 +1,28 @@
 export const getTotalCost = () => {
 	const medicines = document.querySelectorAll('.input_meds');
+	const discounts = document.querySelectorAll('.discount_fields');
 	const totalCost = document.querySelector('.total_cost');
 	let total = 0;
 
-	medicines.forEach((medicine) => {
-		total += +medicine.options[medicine.selectedIndex].dataset.cost;
+	if (!medicines.length) return;
+
+	medicines.forEach((medicine, index) => {
+		total +=
+			+medicine.options[medicine.selectedIndex].dataset.cost *
+			(1 - discounts[index].value / 100);
 	});
 	totalCost.value = total;
 };
 
-window.updateCost = function (e) {
-	e.preventDefault();
-	getTotalCost();
+export const handleEvents = () => {
+	const medicines = document.querySelectorAll('.input_meds');
+	const discounts = document.querySelectorAll('.discount_fields');
+	const l = medicines.length;
+
+	for (let i = 0; i < l; i++) {
+		if (!medicines[i].onchange) {
+			medicines[i].onchange = getTotalCost;
+			discounts[i].onchange = getTotalCost;
+		}
+	}
 };
