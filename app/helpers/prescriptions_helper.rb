d985c4f8acadd00a8edc4690c1871496a8a301e1
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-23 15:29:33 +0800

    Moved grouped_medicine_options to Prescriptions helper Add get_medicine and compute discount methods

--- app/helpers/prescriptions_helper.rb
+++ app/helpers/prescriptions_helper.rb
@@ -1,2 +1,17 @@
 module PrescriptionsHelper
+  def grouped_medicine_options
+    Medicine.all.map do |m|
+      [m.meds_category, "#{m.meds_name} - Php #{m.meds_cost}", m.id, { 'data-cost': m.meds_cost }]
+    end.group_by(&:shift)
+  end
+
+  def get_medicine(presc_medicine)
+    medicine = Medicine.find(presc_medicine.medicine_id)
+    medicine.meds_name
+  end
+
+  def compute_discount(presc_medicine)
+    medicine = Medicine.find(presc_medicine.medicine_id)
+    medicine.meds_cost * (1 - presc_medicine.discount / 100.0)
+  end
 end
