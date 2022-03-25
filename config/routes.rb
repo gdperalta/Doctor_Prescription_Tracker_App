bb0709c73f899c9d13e3e8cdaca176a436aa3282
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-26 01:53:49 +0800

    Add path for all_index prescriptions

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       # delete 'presc_medicines/:id', to: 'presc_medicines#destroy'
     end
   end
+  get 'prescriptions', to: 'prescriptions#all_index', as: 'prescriptions'
 
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
