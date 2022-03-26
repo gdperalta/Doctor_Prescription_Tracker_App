a7e0bb06190a8a87981c421dc957b30acac5e072
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-26 09:59:20 +0800

    Add index controller for home and Add a home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @prescriptions = Prescription.all
+    @medicines = Medicine.all
+    @doctors = Doctor.all
+  end
 end
