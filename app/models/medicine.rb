bd88785cc3ac9091babb4395754874cb969db08b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-30 13:37:58 +0800

    Add has_many through relationships for prescription and medicine

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,5 +1,6 @@
 class Medicine < ApplicationRecord
   has_many :presc_medicines
+  has_many :prescriptions, through: :presc_medicines
   validates :meds_name, presence: true
   validates :meds_cost, presence: true
 end
--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,5 +1,6 @@
 class Prescription < ApplicationRecord
   belongs_to :doctor
   has_many :presc_medicines
+  has_many :medicines, through: :presc_medicines
   accepts_nested_attributes_for :presc_medicines, allow_destroy: true, reject_if: :all_blank
 end
