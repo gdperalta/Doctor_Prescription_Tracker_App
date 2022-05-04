fcecd4d34b0a6801742f843c479b42a166f21b9b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-04 13:57:20 +0800

    Add total_cost method to prescription model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,4 +3,16 @@ class Prescription < ApplicationRecord
   has_many :presc_medicines
   has_many :medicines, through: :presc_medicines
   accepts_nested_attributes_for :presc_medicines, allow_destroy: true, reject_if: :all_blank
+
+  def total_cost
+    total = 0
+    presc_medicines.each do |presc_meds|
+      total +=
+        presc_meds.medicine.meds_cost *
+        presc_meds.quantity *
+        (1 - presc_meds.discount / 100.0)
+    end
+
+    total
+  end
 end
