195546f3d8ce9a965d8ab34ebaed87d8933a584b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-04 14:36:29 +0800

    Removed compute_discount method in prescription_helper Add discounted_price method in PrescMedicine method as alternative

--- app/helpers/prescriptions_helper.rb
+++ app/helpers/prescriptions_helper.rb
@@ -4,9 +4,4 @@ module PrescriptionsHelper
       [m.meds_category, "#{m.meds_name} - Php #{m.meds_cost}", m.id, { 'data-cost': m.meds_cost }]
     end.group_by(&:shift)
   end
-
-  def compute_discount(presc_medicine)
-    medicine = Medicine.find(presc_medicine.medicine_id)
-    presc_medicine.quantity * medicine.meds_cost * (1 - presc_medicine.discount / 100.0)
-  end
 end
--- app/models/presc_medicine.rb
+++ app/models/presc_medicine.rb
@@ -1,4 +1,8 @@
 class PrescMedicine < ApplicationRecord
   belongs_to :prescription
   belongs_to :medicine
+
+  def discounted_price
+    quantity * medicine.meds_cost * ((1 - discount / 100.0))
+  end
 end
